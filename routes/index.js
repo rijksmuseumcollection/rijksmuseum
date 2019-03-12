1e1fc4ef3e999e1f764900d13592f69fbe8e848a
Cristina Blanco <cristinablanco@MacBook-Pro-de-Cristina-5.local>
2019-03-12 12:15:06 +0100

    views

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const axios = require('axios')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  
-  res.render('index')
-  // axios.get("https://www.rijksmuseum.nl/api/en/collection?key=VYUGobm8&format=json&q=van+gogh") 
-  //   .then(response => {console.log(response.data.artObjects); res.render('index', {'data': response.data.artObjects})})
-  //   .catch(err => console.log(err))
+
+    res.render('index')
+        // axios.get("https://www.rijksmuseum.nl/api/en/collection?key=VYUGobm8&format=json&q=van+gogh") 
+        //   .then(response => {console.log(response.data.artObjects); res.render('index', {'data': response.data.artObjects})})
+        //   .catch(err => console.log(err))
 });
 
+
+//ruta para artpiece.hbs
+Router.post('/artpiece')
 
 module.exports = router;
