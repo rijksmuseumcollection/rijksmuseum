847b2f5f31ca91d063468327638bd181e9064549
Cristina Blanco <cristinablanco@MacBook-Pro-de-Cristina-5.local>
2019-03-12 12:10:58 +0100

    añadidas views

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
 
-const session    = require("express-session");
+const session = require("express-session");
 const MongoStore = require('connect-mongo')(session);
-const flash      = require("connect-flash");
-    
+const flash = require("connect-flash");
+
 
 mongoose
-  .connect('mongodb://localhost/rijksmuseum', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+    .connect('mongodb://localhost/rijksmuseum', { useNewUrlParser: true })
+    .then(x => {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    })
+    .catch(err => {
+        console.error('Error connecting to mongo', err)
+    });
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -37,11 +37,11 @@ app.use(cookieParser());
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -50,15 +50,15 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 hbs.registerHelper('ifUndefined', (value, options) => {
-  if (arguments.length < 2)
-      throw new Error("Handlebars Helper ifUndefined needs 1 parameter");
-  if (typeof value !== undefined ) {
-      return options.inverse(this);
-  } else {
-      return options.fn(this);
-  }
+    if (arguments.length < 2)
+        throw new Error("Handlebars Helper ifUndefined needs 1 parameter");
+    if (typeof value !== undefined) {
+        return options.inverse(this);
+    } else {
+        return options.fn(this);
+    }
 });
-  
+
 
 // default value for title local
 app.locals.title = 'Rijks Museum Api';
@@ -66,14 +66,14 @@ app.locals.title = 'Rijks Museum Api';
 
 // Enable authentication using session + passport
 app.use(session({
-  secret: 'irongenerator',
-  resave: true,
-  saveUninitialized: true,
-  store: new MongoStore( { mongooseConnection: mongoose.connection })
+    secret: 'irongenerator',
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
 }))
 app.use(flash());
 require('./passport')(app);
-    
+
 
 const index = require('./routes/index');
 app.use('/', index);
@@ -83,5 +83,8 @@ app.use('/auth', authRoutes);
 
 const albums = require('./routes/albums')
 app.use('/albums', albums)
+
+const search = require('./routes/search')
+app.use('/search', search)
 
 module.exports = app;
--- models/Artpiece.js
+++ models/Artpiece.js
@@ -1,19 +1,18 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const artPieceSchema = new Schema({
-  image: String,
-  title: String,
-  description: String,
-  author: String,
-  type: String,
-  material: String,
-  technique: String
+    image: String,
+    title: String,
+    description: String,
+    author: String,
+    age: Date,
+    technique: String
 }, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
-  }
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
 });
 
 const ArtPiece = mongoose.model('ArtPiece', artPieceSchema);
