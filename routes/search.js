885e9867bda7c21d36bc6598cf52b79390c02921
Cristina Blanco <cristinablanco@MacBook-Pro-de-Cristina-5.local>
2019-03-15 11:11:11 +0100

    limite de busqueda

--- routes/search.js
+++ routes/search.js
@@ -1,13 +1,13 @@
 const express = require('express')
-const router  = express.Router()
+const router = express.Router()
 const axios = require('axios')
 
 
 router.post('/', (req, res) => {
-  const {search} = req.body
-  axios.get(`https://www.rijksmuseum.nl/api/en/collection?key=VYUGobm8&format=json&q=${search}&ps=10`)
-    .then(response => res.render('search', {response: response.data.artObjects}))
-    .catch(err => console.log(err))
+    const { search } = req.body
+    axios.get(`https://www.rijksmuseum.nl/api/en/collection?key=VYUGobm8&format=json&q=${search}&ps=20`)
+        .then(response => res.render('search', { response: response.data.artObjects }))
+        .catch(err => console.log(err))
 })
 
 module.exports = router
